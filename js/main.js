f6c9b116e9ab2ceb3d51df1115114897615ef5f7
Ayrton Sousa <ayrtontisolution@hotmail.com.br>
2021-07-20 13:28:01 -0300

    Diagnosis Shown
    Diagnosis wasn't showing it the front-end, this commit contains the fix.

--- js/main.js
+++ js/main.js
@@ -17,7 +17,8 @@ var app = new Vue({
         {name:"Brabinha", age:49, phone:"+55xx x xxxx xxxx", email:"brabinha123@brabinha.com"} 
       ],
       breastCancerLabels : ["radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean", "compactness_mean", "concavity_mean","concave_points_mean","symmetry_mean", "fractal_dimension_mean", "radius_se", "texture_se",  "perimeter_se", "area_se", "smoothness_se", "compactness_se", "concavity_se", "concave_points_se", "symmetry_se", "fractal_dimension_se", "radius_worst", "texture_worst", "perimeter_worst", "area_worst", "smoothness_worst", "compactness_worst", "concavity_worst", "concave_points_worst", "symmetry_worst", "fractal_dimension_worst"],
-      info : ''
+      info : '',
+      message : ''
     },
     methods:{
       sendExamToLuna : () => {
@@ -56,10 +57,8 @@ var app = new Vue({
              "fractal_dimension_worst":document.getElementById("fractal_dimension_worst").value
           }
         }).then(response => {
-        this.info = response
-        console.log(this.info);
+          document.getElementById('result').innerHTML = response.data.message;
         });
-        
 
       },  
     }
